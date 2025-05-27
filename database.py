4601e1e6428f84bbbd89649ae334a99fc15193c8
noName <samyrize77777@gmail.com>
2025-05-27 19:53:13 +0400

    req

--- database.py
+++ database.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = "postgresql://postgres.ehnguyuzpffqzaednrnn:z8O26qYv4Perh997@aws-0-eu-west-2.pooler.supabase.com:6543/postgres"
+DATABASE_URL = os.getenv("SUPABASE_DB_URL")
 database = Database(DATABASE_URL)
